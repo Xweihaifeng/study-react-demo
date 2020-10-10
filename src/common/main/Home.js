16baa5956221c9a96b5c0fc5cf757bbf81b28d6b
Wei <hfweic@isoftstone.com>
2020-10-10 18:25:39 +0800

    class

--- src/common/main/Home.js
+++ src/common/main/Home.js
@@ -1,9 +1,46 @@
 import React from 'react';
 
 var myStyle = { color: 'red', textAlign: 'center' }
+class Navmenu extends React.Component {
+    constructor() {
+        super();
+        this.state = {}
+    }
+    render() {
+        return (
+            <ul>
+                <li>全景展示</li>
+                <li>高校防控</li>
+                <li>应急指挥</li>
+                <li>交通运行</li>
+                <li>系统运营</li>
+            </ul>
+        );
+    }
+}
 class Welcome extends React.Component {
+    constructor() {
+        super();
+        this.state = {}
+    }
     render() {
         return <h1>Hello World!</h1>;
+    }
+}
+class Headers extends React.Component {
+    constructor() {
+        super();
+        this.state = {}
+    }
+    render() {
+        return (
+            <section style={myStyle}>
+                <header>
+                    <Navmenu />
+                    <Welcome />
+                </header>
+            </section>
+        );
     }
 }
 export default class Home extends React.Component {
@@ -11,25 +48,10 @@ export default class Home extends React.Component {
         super(props);
         this.state = {};
     }
-
     isClickDel(e) {
         console.log(e);
     }
-
     render() {
-        return (
-            <section style={myStyle}>
-                <header>
-                    <ul>
-                        <li>全景展示</li>
-                        <li>高校防控</li>
-                        <li>应急指挥</li>
-                        <li>交通运行</li>
-                        <li>系统运营</li>
-                    </ul>
-                    <Welcome />
-                </header>
-            </section>
-        )
+        return <Headers />
     }
 }
